2bc3ab6bd6306339bc5b1342d1eeb1cfe7341139
Joey-Foster <127777617+Joey-Foster@users.noreply.github.com>
2024-09-18 16:15:48 +0100

    implemented unreasonably precise convergence

--- FTBSconvergence.py
+++ FTBSconvergence.py
@@ -27,7 +27,7 @@ def EndPointRMSCalculator(nx,scaling,u,initial_data):
     nt=scaling*nx
     dt=1/nt
     
-    #print("CFL = ", u*dt/dx)
+    print("CFL = ", u*dt/dx)
     
     #initial conditions
     phi = initial_data(x)
@@ -35,15 +35,21 @@ def EndPointRMSCalculator(nx,scaling,u,initial_data):
 
     def analytic(x,t):
         return initial_data(x-u*t)
+    
+    #Vectorized FTBS
+    for t in range(nt + 1):
+        phi[1:] = phiOld[1:] - u*dt/dx * (phiOld[1:] - phiOld[:-1])
+        phi[0] = phi[-1]
+        phiOld[:] = phi[:]
    
     #FTBS scheme
-    for t in range(nt+1):
-        for j in range(1,nx+1):
-            phi[j]=phiOld[j]-u*dt/dx*(phiOld[j]-phiOld[j-1])
-        phi[0]=phi[-1]
-        phiOld = phi.copy()
+    # for t in range(nt+1):
+    #     for j in range(1,nx+1):
+    #         phi[j]=phiOld[j]-u*dt/dx*(phiOld[j]-phiOld[j-1])
+    #     phi[0]=phi[-1]
+    #     phiOld = phi.copy()
         
-       # print(t, t*dt)
+        # print(t, t*dt)
         # plt.cla()
         # plt.plot(x,phi,'b',label='Time_'+str(np.around(t*dt,2)))
         # plt.plot(x,analytic(x,(t+1)*dt),'r',label='analytic')
@@ -53,22 +59,24 @@ def EndPointRMSCalculator(nx,scaling,u,initial_data):
         # plt.pause(0.01)
         
     
-    AnalyticAtEnd = analytic(x, (nt+1)*dt)
-    NumericalAtEnd = phi
+    AnalyticAtEnd = analytic(x, (nt+1)*dt)[:-1]
+    NumericalAtEnd = phi[:-1]
     
-   # return np.linalg.norm(abs(AnalyticAtEnd-NumericalAtEnd),2) 
+    #return np.linalg.norm(abs(AnalyticAtEnd-NumericalAtEnd),2) 
     return np.sqrt(np.mean((AnalyticAtEnd-NumericalAtEnd)**2))
 
-#print(EndPointRMSCalculator(4, 1, 0.9, phi0))
+print(EndPointRMSCalculator(100, 2, 2, phi0))
 # print(EndPointRMSCalculator(200, 1, 0.9, phi0))
 
 #%% Parameters of simulation
 CFL=0.9
 scaling = 2
 
-nxs= 2**(np.arange(5,15))
+nxs= 2**(np.arange(8,18))
 nts = scaling*nxs
-u=CFL/scaling
+
+u=CFL*scaling
+
 
 #%% Run error for loop
 
@@ -91,23 +99,19 @@ def trendline(data,x):
     return np.poly1d(data)(x)
 
 #%% doPlotting
+plt.figure(1)
+fig, ax = plt.subplots(1,1,figsize=(fig_width_inches,fig_height_inches))
+plt.loglog(dxs,np.exp(trendline(errors_polyfit_coeffs,np.log(dxs))),'-r',label=rf'$\propto {{\Delta x}}^{{{errors_polyfit_coeffs[0]:.2f}}}$')
+plt.loglog(dxs, errors, 'x',lw=2,label=r'Error wrt $\Delta x$',c='k')
+plt.loglog(dts,np.exp(trendline(errors2_polyfit_coeffs,np.log(dts))),'--',c='orange',label=rf'$\propto {{\Delta t}}^{{{errors2_polyfit_coeffs[0]:.2f}}}$')
+plt.loglog(dts, errors, '.',lw=2,label=r'Error wrt $\Delta t$',c='b')
+plt.xlabel('Discretisation Step Size')
+plt.ylabel(r'$\mathrm{RMS(Error)}$')
+#plt.set_title(r'Error against $\Delta x$ and $\Delta t$')
+plt.legend()
 
-fig, ax = plt.subplots(1,2,figsize=(fig_width_inches,fig_height_inches))
-ax[0].loglog(dxs,np.exp(trendline(errors_polyfit_coeffs,np.log(dxs))),'-r',label=rf'$\propto {{\Delta x}}^{{{np.around(errors_polyfit_coeffs[0],1)}}}$')
-ax[0].loglog(dxs, errors, 'x',lw=2,label='Error',c='k')
-ax[0].set_xlabel(r'$\Delta x$')
-ax[0].set_ylabel(r'$\left\Vert\mathrm{Error}\right\Vert_{2}$')
-ax[0].set_title(r'Error against $\Delta x$')
-ax[0].legend()
+plt.title(rf"Courant number $\mu$ = {CFL}, wave speed a = {u}",y=1.02)
 
-ax[1].loglog(dts,np.exp(trendline(errors2_polyfit_coeffs,np.log(dts))),'-r',label=rf'$\propto {{\Delta t}}^{{{np.around(errors2_polyfit_coeffs[0],1)}}}$')
-ax[1].loglog(dts, errors, 'x',lw=2,label='Error',c='k')
-ax[1].set_xlabel(r'$\Delta t$')
-#ax[1].set_ylabel(r'$\left\Vert\mathrm{Error}\right\Vert_{2}$')
-ax[1].set_title(r'Error against $\Delta t$')
-ax[1].legend()
-
+plt.savefig("FTBScourant=0.9.pdf", format="pdf", bbox_inches="tight")
 
 plt.show()
-#plt.savefig("figure3.pdf", format="pdf", bbox_inches="tight")
-
